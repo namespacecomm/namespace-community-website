2d7f06e4d5e0c704a532fea47adb459719fe1af4
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-10-02 12:39:46 +0530

    programs details added

--- src/constants/programs.js
+++ src/constants/programs.js
@@ -1,14 +1,17 @@
 export const programsList = [
     {
-        name: TechRumble,
-        description:"",
+        name: "AlgoRumble",
+        description:"AlgoRumble is NSCC's weekly Data Structures and Algorithms contest, an engaging event designed to test participants' problem-solving skills and algorithmic ability.",
+        image: "./img/internal-programs/algorumble.png",
     },
     {
-        name: AlgoRumble,
-        description:"",
+        name: "TechRumble",
+        description:"TechRumble is where coding meets creativity! In this, students get to alternate between thrilling tasks in Data Structures and Algorithms and the dynamic world of Web Development. The completed tasks are then pushed to the participants’ GitHub to build their portfolio of projects.",
+        image: "./img/internal-programs/techrumble.png",
     },
     {
-        name: TechTrek,
-        description:"",
+        name: "TechTrek",
+        description:"TechTrek is a series of interactive sessions on various technology topics to enrich knowledge and skills taken by seniors of NSCC. From cutting-edge advancements to practical applications, each session is carefully curated to enrich knowledge and expand horizons.",
+        image: "./img/internal-programs/techtrek.png"
     },
 ];
