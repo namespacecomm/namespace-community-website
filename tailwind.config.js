06e4d7f413b23ff3a297b5823362a596ae6b9363
Vinoth-git-17 <vinothnick17@gmail.com>
2023-10-02 15:17:46 +0530

    Enhancement of Home UI by Changing fonts and adjusting layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ module.exports = {
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
       },
+      fontFamily :{
+        Inter : ['Inter', 'sans-serif'],
+        Inter : ['Plus Jakarta Sans', 'sans-serif'],
+      },
       screens: {
         xs: "450px",
       },
