01fcf299b0b9f118e182ae50d8d4c20302022979
Harsha <c.harsha0909@gmail.com>
2023-10-06 20:52:27 +0530

    Programs page UI updated

--- src/Pages/Programs/Programs.jsx
+++ src/Pages/Programs/Programs.jsx
@@ -17,10 +17,14 @@ const Container = styled.div`
     padding-top: 100px;
     @media only screen and (max-width: 768px) {
         width: 100%;
-
         flex-direction: column;
-
         justify-content: space-between;
+    }
+`;
+
+const ProgramDescription = styled.div`
+    @media only screen and (max-width: 768px) {
+        margin-top: 1rem;
     }
 `;
 
@@ -50,21 +54,21 @@ function Programs() {
                                             className="overflow-hidden shadow-xl shadow-cyan-300 bg-slate-950 rounded text-white p-4"
                                             key={item.name}
                                         >
-                                            <div className="text-center text-xl md:text-2xl lg:text-5xl font-semibold mb-12 mt-8">
+                                            <div className="text-center text-xl md:text-2xl lg:text-5xl font-bold mb-12 mt-8">
                                                 {item.name}
                                             </div>
 
                                             <div className="program-content flex flex-col md:flex-row">
-                                                <div className="program-image md:w-1/2 pr-4">
+                                                <div className="program-image md:w-1/2 md:pr-4">
                                                     <img
                                                         src={item.image}
                                                         alt={item.name}
                                                         className="w-full rounded-md"
                                                     />
                                                 </div>
-                                                <div className="program-description border-2 border-slate-600 rounded-md p-4 md:w-1/2 mt-4 md:mt-0 h-48 md:h-auto md:text-xl lg:text-3xl">
+                                                <ProgramDescription className="program-description flex items-center border-2 border-slate-600 rounded-md p-4 md:w-1/2 md:h-auto md:text-sm lg:text-xl xl:text-2xl">
                                                     <p>{item.description}</p>
-                                                </div>
+                                                </ProgramDescription>
                                             </div>
                                         </div>
                                     );
