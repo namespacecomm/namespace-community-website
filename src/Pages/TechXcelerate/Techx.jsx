06013cc6d64bc97e9afa79f653090579cb027a4d
Pradeepto Sarkar <psrcofficial10@gmail.com>
2024-01-01 10:49:08 +0530

    techx page added

---
+++ src/Pages/TechXcelerate/Techx.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import styled from "styled-components";
+import { allmembers } from "../../constants/constants";
+import TeamCard from "../../Sections/Teams/TeamCard";
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  display: flex;
+  ${
+    "" /* background-color: rgb(9, 9, 121);
+  background-repeat: no-repeat;
+  background: linear-gradient(
+    262deg,
+    rgba(9, 9, 121, 1) 0%,
+    rgba(2, 0, 36, 1) 1%
+  ); */
+  }
+  background-color: #010116;
+  ${
+    "" /* background-repeat: no-repeat;
+  background: no-repeat url("./img/bg.jpg"); */
+  }
+`;
+
+const Section2 = styled.div`
+  height: 100%;
+  background-color: #010116;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 2rem 0;
+`;
+
+const YoutubeFrame = styled.iframe`
+  width: 30vw;
+  height: 350px;
+
+  @media only screen and (max-width: 768px) {
+    width: 90vw;
+    height: 250px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+`;
+
+const Container = styled.div`
+  scroll-snap-align: center;
+  width: 1440px;
+  padding-top: 100px;
+  @media only screen and (max-width: 738px) {
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+  }
+  @media only screen and (max-width: 1030px) {
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media only screen and (max-width: 1030px) {
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+function TechX() {
+  return (
+    <>
+      <Section>
+        <Container>
+          <div className=" px-6 py-10 ">
+            <h2 className="text-3xl font-bold leading-tight text-transparent bg-clip-text  bg-gradient-to-r from-teal-400 to-yellow-200 sm:text-4xl lg:text-5xl text-center">
+              TechXcelerate
+            </h2>
+
+            <p className="max-w-4xl mx-auto my-10 text-center text-xl font-bold leading-tight text-transparent bg-clip-text  bg-gradient-to-r from-amber-500 to-pink-500 ">
+              Our society launched TechXcelerate, in April this year, a series
+              of interactive sessions aimed at first and second year students.
+              These sessions feature beginner level topics and the necessary
+              tips and tricks to learn the various technical domains covering
+              Competitive Programming, web development, app development, data
+              science etc. The very first edition saw participation from 1,200
+              unique students from various colleges across India, with 15+ hours
+              of live interaction. TechXcelerate 2023 generated more than 5,000
+              impressions across social media channels in only 10 days and the
+              upcoming edition in Jan 2024 will surely be a huge improvement
+              from that.
+            </p>
+
+            <div className="mx-auto flex flex-col gap-3">
+              <div className="flex items-center justify-between flex-wrap gap-5">
+                <h4 className="font-bold md:text-[64px] text-[44px] text-white mt-[100px]">
+                  Register for TechXcelerate 2.0
+                </h4>
+              </div>
+            </div>
+
+            <div style={{ marginTop: "50px" }}>
+              <iframe
+                title="Unique Title for Accessibility"
+                src="https://lu.ma/embed-checkout/evt-OQPjFH6fJtUOEA4"
+                width="100%"
+                height="800"
+                style={{
+                  border: "1px solid #bfcbda88",
+                  borderRadius: "24px",
+                  maxWidth: "", // Set maximum width if needed
+                  margin: "0 auto", // Center the iframe
+                  boxShadow: "10px 10px 20px rgba(0, 0, 0, 0.9)", // Example box shadow
+                }}
+                allowfullscreen=""
+                aria-hidden="false"
+              />
+            </div>
+
+            <>
+              <h2 className="mt-[100px] text-2xl text-center md:text-4xl lg:text-5xl font-bold">
+                View Past{" "}
+                <span className="text-2xl md:text-4xl lg:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800">
+                  <a href="https://linktr.ee/nscc_bpit">Sessions</a>
+                </span>
+              </h2>
+
+              <div className="flex items-center justify-center mt-4">
+                <a
+                  href="https://drive.google.com/drive/folders/1eVfG4LUVbiEJ02F46odyqiRfhX342PXW?usp=sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    type="button"
+                    className="text-white mt-1 bg-blue-700 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-0 mb-2"
+                  >
+                    Access Previous Year PPTs
+                  </button>
+                </a>
+              </div>
+
+              <div className="flex items-center justify-center">
+                <a
+                  href="https://www.youtube.com/@namespacecomm"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    type="button"
+                    className="text-white mt-1 bg-blue-700 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-0 mb-2"
+                  >
+                    Our YouTube Channel
+                  </button>
+                </a>
+              </div>
+              <Section2>
+                <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center">
+                  <YoutubeFrame src="https://www.youtube.com/embed/eWc6eVcaLyw?si=QA9_CUfmkNKABWT0" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/aezbGftYJhA?si=8JrrimqBNSNcmiaW" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/M6ZLkN-Hvww?si=8CZSs0VrLxFtSYlE" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/qS8K2hVPfm0?si=-az_q1omrXTmO5vJ" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/I2AvuoJVpIo?si=SfEHF17CkvlcOoxn" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/Lu5l4THO5GM?si=MtrbhG8czkmBFMSi" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/MWXZCJJbdk4?si=qacDkoxUnLPo_G5G" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/SLT30RUO-2I?si=2EsMnq-vwz9hSRvl" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/36cxpDBBHlU?si=n_T_kW6Hk49qyxSx" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/PzAiSJdDO7o?si=vgyQslC7TS0vQh1I" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/hXA1l1by-vE?si=Fnw8tcAuj85TetR2" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/ZPc-MAv-6Kk?si=qpWXMKQAJ-mu7ACS" />
+                  <YoutubeFrame src="https://www.youtube.com/embed/dnSgiQffc6o?si=jvnDkPUgx_jRXpWK" />
+                </div>
+              </Section2>
+            </>
+          </div>
+        </Container>
+      </Section>
+    </>
+  );
+}
+
+export default TechX;
