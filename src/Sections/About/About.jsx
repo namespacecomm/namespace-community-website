40a375a1abe65072aa877a9b0f4cd2b56bd13e97
vrindabindal12 <vrindabindal12@gmail.com>
2024-07-25 16:37:19 +0530

    issue 555 resolved

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -20,7 +20,7 @@ const Section = styled.div`
   ${"" /* background: no-repeat url("./img/bg.jpg"); */}
   background-color: #010116;
   @media only screen and (max-width: 768px) {
-    height: 100vh;
+    height: 150vh;
   }
 `;
 
