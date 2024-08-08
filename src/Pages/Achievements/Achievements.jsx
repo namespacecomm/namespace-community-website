5513847adbea0c57a21022fdf2b09235427832b8
Arsh Tiwari <dchglucknow@gmail.com>
2024-08-08 21:06:34 +0530

    added para in achievements section , spelling error in hackhazards corrected , enhanced the cards look in achievements page

--- src/Pages/Achievements/Achievements.jsx
+++ src/Pages/Achievements/Achievements.jsx
@@ -9,6 +9,11 @@ const Achievements = () => {
       <h2 className="text-3xl font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-yellow-200 sm:text-4xl lg:text-5xl mt-16 items-center text-center">
         OUR ACHIEVERS
       </h2>
+
+      <p className="max-w-2xl px-3 mx-auto my-10 text-center flex justify-center items-center text-xl font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-r from-amber-500 to-pink-500">
+  At nameSpace, we take pride in the remarkable accomplishments of our members, who consistently push the boundaries of innovation and excellence. This section celebrates the dedication, hard work, and success of those who have made significant contributions both within our society and beyond. From pioneering projects to winning prestigious awards, our achievers exemplify the true spirit of nameSpace. Explore the milestones and achievements that define our journey and inspire future generations.
+</p>
+
       
       <div className="flex flex-wrap justify-center">
         {achievements.map((event, index) => (
