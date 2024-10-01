707d35f5ab3ccf260bcffdf2c22465648ccef396
krishna <krishna@krishna.home>
2024-10-01 16:01:24 +0530

    fixed the broken image url

--- src/Pages/AllResources/devopsALL/kubernetes.jsx
+++ src/Pages/AllResources/devopsALL/kubernetes.jsx
@@ -159,7 +159,7 @@ export const KUBERNETES = () => {
 const resources = [
   {
     title: "Kubernetes - Docs",
-    image: "https://kubernetes.io/images/favicon.png",
+    image: "https://images.ctfassets.net/23aumh6u8s0i/Gbd2bxUf83mbfw0QhGKF4/67c00e8fce936143425b965870123e1c/kubernetes",
     type: "Documentation",
     link: "https://kubernetes.io/docs/home/",
   },
