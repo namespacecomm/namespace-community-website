029883abfd5233168eda6824a8dcc158b4f3ccef
Lavavarshney <lavanya.varshney2104@gmail.com>
2024-10-01 22:22:13 +0530

    Added Faq section

---
+++ src/Sections/Faq/faqData.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+const faqData = [
+  {
+    id: 1,
+    question: "Why join nameSpace Community?",
+    answer: "Joining the nameSpace Community provides you with an opportunity to connect with like-minded tech enthusiasts, collaborate on innovative projects, and gain access to valuable resources and mentorship. Whether you're looking to enhance your skills, stay updated with the latest industry trends, or contribute to open-source projects, nameSpace offers a supportive environment to grow both personally and professionally. You'll also have the chance to network with professionals and peers, participate in events, and build a portfolio of real-world tech experience."
+  },
+    {
+      id: 2,
+      question: "Who should join nameSpace Community?",
+      answer: "The nameSpace Community is open to any college student with a passion for technology, whether you're a beginner or an experienced professional. Students, developers, designers, tech enthusiasts, and innovators are all welcome to join."
+    },
+    
+      {
+      id: 3,
+      question: "Is there a membership fee?",
+      answer: "No, there’s no membership fee to join the our community! We believe in fostering an inclusive environment where everyone can participate freely. Join us, and become part of our vibrant community!"
+    },
+ 
+    {
+      id: 4,
+      question: "I could not be a member of the community...what is in store for me?",
+      answer: "Even if you not a member,we got you covered with our public events like Techxcelerate and Hachazards.Go read about them in our Events page :)"
+    }
+  ];
+  export default faqData;
