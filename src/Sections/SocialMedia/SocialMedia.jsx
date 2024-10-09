0ed57c361255241579c9e7e9f69e89f5eb9798c5
anuj846k <anuj846k@gmail.com>
2024-10-09 21:15:20 +0530

    Responsiveness of Faq and SocialMedia section Fixed

--- src/Sections/SocialMedia/SocialMedia.jsx
+++ src/Sections/SocialMedia/SocialMedia.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { YouTubeEmbed } from "react-social-media-embed";
 import styled from "styled-components";
 import { socialMediaUrls } from "../../utils/constants";
 
@@ -10,15 +9,6 @@ const Section = styled.div`
   flex-direction: column;
   width: 100%;
   margin: 2rem 0;
-`;
-
-const YoutubeFrame = styled(YouTubeEmbed)`
-  
-
-  @media only screen and (max-width: 768px) {
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
 `;
 
 const SocialMedia = () => {
@@ -32,22 +22,24 @@ const SocialMedia = () => {
       </h2>
       <Section>
         <div className="flex flex-col">
-          {socialMediaUrls.map(social => {
-            const Component = social.component
+          {socialMediaUrls.map((social, index) => {
+            const Component = social.component;
 
             return (
-              <div className="my-5 flex flex-col gap-6">
+              <div key={index} className="my-5 flex flex-col gap-6">
                 <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center">
                   <h3 className="text-2xl md:text-4xl lg:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800">
                     {social.name}
                   </h3>
                 </div>
 
-                <div className="flex justify-center items-center gap-20">
-                  {social.embedUrls.map(item => <Component width={350} height={250} {...item} />)}
+                <div className="flex flex-col md:flex-row justify-center items-center gap-20">
+                  {social.embedUrls.map((item, index) => (
+                      <Component {...item} key={index} width={310} height={250} />
+                  ))}
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       </Section>
