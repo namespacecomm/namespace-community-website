d93741f53087074f5614b147ee9743cbcebf5720
Mahesh <maheshkolli888@gmail.com>
2024-10-18 22:43:59 +0530

    added cards in Chapter

--- src/Pages/Chapters/Chapters.jsx
+++ src/Pages/Chapters/Chapters.jsx
@@ -1,39 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
+import ChapterCard from './ChapterCard';
+import chaptersData from './chapters.json';
 
 const Section = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  display: flex;
+  background-color: #0f0f1e;
+  min-height: 100vh;
 `;
-
 
 const Container = styled.div`
   scroll-snap-align: center;
-  width: 1440px;
-  padding-top: 100px;
+  width: 100%;
+  max-width: 1440px;
+  padding: 100px 20px;
 `;
 
 const Title = styled.h2`
-  text-align: center;
   font-size: 3rem;
-  font-weight: bold;
-  background: linear-gradient(to right, #4fd1c5, #f6e05e);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  color: #e94560;
+  text-align: center;
+  margin-bottom: 2rem;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+`;
+
+const ChaptersGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 2rem;
   padding: 2rem 0;
 `;
 
-function Chapters () {
+function Chapters() {
   return (
     <Section>
       <Container>
-        <Title>namespace Chapters</Title>
+        <Title style={{ marginTop: "100px" }}>Namespace Chapters</Title>
+        <ChaptersGrid>
+          {chaptersData.map((chapter) => (
+            <ChapterCard key={chapter.id} chapter={chapter} />
+          ))}
+        </ChaptersGrid>
       </Container>
     </Section>
   );
-};
+}
 
 export default Chapters;
